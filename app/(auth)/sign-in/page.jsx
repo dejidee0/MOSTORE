4dabc77fdabc47342cf592d7594cdeff1807e9ce
Gracious-kingsley <Kingsleygracious16@gmail.com>
2025-06-11 18:07:49 +0100

    added supabase,clerk and few functions

--- app/(auth)/sign-in/page.jsx
+++ app/(auth)/sign-in/page.jsx
@@ -1,40 +1,57 @@
 "use client";
-
-import Image from "next/image";
-import React from "react";
-
+import React, { useState } from "react";
+import { useSignIn } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 const SignInPage = () => {
+  const { signIn, isLoaded } = useSignIn();
+  const router = useRouter();
+
+  const [form, setForm] = useState({ email: "", Password: "" });
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    if (!isLoaded || isLoading) return;
+
+    if (!form.email || !form.Password) {
+      setError("All fields are required.");
+      return;
+    }
+    try {
+      setIsLoading(true);
+      setError("");
+      const result = await signIn.create({
+        identifier: form.email,
+        password: form.Password,
+      });
+      if (result.status === "complete") {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error(error);
+      setError(error.errors?.[0]?.message || "sign-in faild.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <div className="min-h-screen flex flex-col justify-center md:flex-row font-raleway bg-white">
+    <div className="min-h-screen flex flex-col md:flex-row font-raleway">
+      {/* Left Side Image */}
       <div
         className="hidden md:flex md:w-1/2 bg-cover bg-center"
         style={{ backgroundImage: "url('/auth.png')" }}
       ></div>
 
       {/* Right Side Form */}
-      <div className="flex w-full md:w-1/2 items-center justify-center p-6 sm:p-10">
-        <div className="w-full max-w-md bg-white shadow-xl rounded-lg p-8 md:p-10 space-y-6">
-          <div className="md:hidden flex justify-center">
-            <Image
-              src="/assets/Mostore Logo Icon.png"
-              width={80}
-              height={80}
-              alt="Logo"
-              priority
-            />
-          </div>
-
-          <div className="text-center">
-            <h2 className="text-3xl font-semibold text-gray-800">
-              Welcome Back
-            </h2>
-            <p className="text-sm text-gray-500 mt-1">
-              Please sign in to continue
-            </p>
-          </div>
-
-          {/* Form */}
-          <form className="space-y-5">
+      <div className="flex w-full md:w-1/2 items-center justify-center p-8">
+        <div className="w-full max-w-md">
+          <h2 className="text-3xl font-semibold mb-6 text-center ">Sign In</h2>
+          {error && <p className="text-red-500 text-sm text-center">{error}</p>}
+          <form className="space-y-5" onSubmit={handleSignIn}>
             <div>
               <label
                 htmlFor="email"
@@ -44,7 +61,10 @@ const SignInPage = () => {
               </label>
               <input
                 id="email"
+                 name="email"
                 type="email"
+                value={form.email}
+                onChange={handleChange}
                 required
                 placeholder="you@example.com"
                 className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary text-sm"
@@ -61,6 +81,9 @@ const SignInPage = () => {
               <input
                 id="password"
                 type="password"
+                name="Password" 
+                value={form.Password}
+                onChange={handleChange}
                 required
                 placeholder="••••••••"
                 className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary text-sm"
@@ -83,17 +106,9 @@ const SignInPage = () => {
               type="submit"
               className="w-full bg-primary hover:bg-primary-dark text-white py-2 px-4 rounded-md transition-all duration-200"
             >
-              Sign In
+              {isLoading ? "signing in..." : "Sign In"}
             </button>
           </form>
-
-          {/* Footer */}
-          <p className="text-center text-sm text-gray-500">
-            Don’t have an account?{" "}
-            <a href="/sign-up" className="text-primary hover:underline">
-              Sign Up
-            </a>
-          </p>
         </div>
       </div>
     </div>
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono, Raleway } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,8 +26,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${raleway.variable} font-raleway`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${raleway.variable} font-raleway } antialiased`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }

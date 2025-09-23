e2dddc772fed36f9212b1e2abd4260238c6ad656
Daniel Aboderin <aboderindaniel482@gmail.com>
2025-09-23 18:45:38 +0100

    `Removed home breadcrumb from user breadcrumbs component`

--- components/shared/user/BreadCrumbs.jsx
+++ components/shared/user/BreadCrumbs.jsx
@@ -15,24 +15,6 @@ const Breadcrumbs = ({ activeTab }) => {
     <nav className="flex mb-6 px-4 md:px-0" aria-label="Breadcrumb">
       <ol className="inline-flex items-center space-x-2 text-sm text-gray-600">
         <li className="flex items-center">
-          <Link
-            href="/"
-            className="hover:text-orange-500 transition-colors flex items-center"
-          >
-            <Home className="w-4 h-4 mr-2" />
-            Home
-          </Link>
-        </li>
-        <li className="flex items-center">
-          <ChevronRight className="w-4 h-4 mx-1 text-gray-400" />
-          <Link
-            href="/my-account"
-            className="hover:text-orange-500 transition-colors"
-          >
-            My Account
-          </Link>
-        </li>
-        <li className="flex items-center">
           <ChevronRight className="w-4 h-4 mx-1 text-gray-400" />
           <span className="text-orange-500 font-medium">
             {tabDisplayNames[activeTab] || "My Account"}

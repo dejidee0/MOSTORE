2bd2ed710f71829eedc9521985103ae9bc928c14
Daniel Aboderin <aboderindaniel482@gmail.com>
2025-10-25 08:25:36 +0100

    `Added new image paths to getCategoryImage function`

--- components/shared/Home/CategoriesSection.jsx
+++ components/shared/Home/CategoriesSection.jsx
@@ -22,6 +22,9 @@ const getCategoryImage = (name) => {
     return "https://images.unsplash.com/photo-1746014995761-bf045dc83b0a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fG1vdG9yJTIwb2lsfGVufDB8fDB8fHww";
   if (lower.includes("kids"))
     return "https://images.unsplash.com/photo-1594608661623-aa0bd3a69d98?q=80&w=948&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  if (lower.includes("devices")) return "/hero/phones.jpg";
+  if (lower.includes("appliances")) return "/hero/gadgets.jpg";
+  if (lower.includes("accessories")) return "/hero/accessories.jpg";
   return "https://images.unsplash.com/photo-1558618047-3c8c76ca7d13?w=400&h=300&fit=crop";
 };
 

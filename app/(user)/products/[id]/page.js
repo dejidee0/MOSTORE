daabd6598f43fb3ce4524f5e2a2821851bb37916
Daniel Aboderin <aboderindaniel482@gmail.com>
2025-11-06 20:26:57 +0100

    `Added short product description to product details page`

--- app/(user)/products/[id]/page.js
+++ app/(user)/products/[id]/page.js
@@ -311,6 +311,9 @@ export default function ProductDetails() {
                 ))}
               </div>
             )}
+            <div className="text-gray-800 text-xl leading-relaxed">
+              {product.short_description}
+            </div>
           </div>
 
           <div className="space-y-6">
@@ -370,9 +373,6 @@ export default function ProductDetails() {
 
             <div className="text-gray-600 leading-relaxed">
               {product.description || product.short_description}
-            </div>
-            <div className="text-gray-600 leading-relaxed">
-              {product.short_description}
             </div>
 
             {hasColors && (
